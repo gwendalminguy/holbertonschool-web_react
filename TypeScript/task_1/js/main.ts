02d4a1cb4961eec387ec8dc1e2797b8093d243c7
gwendalminguy <gwendalminguy@outlook.fr>
2025-09-10 15:12:43 +0200

    Created teacher interface with optional attributes

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+const teacher1: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+}
+
+console.log(teacher1);
