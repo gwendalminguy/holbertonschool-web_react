75ab9e175e9b30967bea16bec81367316e6d4e11
gwendalminguy <gwendalminguy@outlook.fr>
2025-09-12 10:52:55 +0200

    Created shared namespace with merged declarations

---
+++ TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,20 @@
+/// <reference path="./Subject.ts" />
+/// <reference path="./Teacher.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Cpp';
+    }
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingC) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
