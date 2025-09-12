873c2da08d6fe71e97854cb0bca8fd9e1f3aeec1
gwendalminguy <gwendalminguy@outlook.fr>
2025-09-12 08:42:29 +0200

    Created type Subjects and function todayClass

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -68,3 +68,16 @@ function executeWork(employee: Teacher | Director): string {
 
 // console.log(executeWork(createEmployee(200)));
 // console.log(executeWork(createEmployee(1000)));
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else {
+    return 'Teaching History';
+  }
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
